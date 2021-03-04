1bd5302fbb66a25b56f2fa2db184ee498fad15b5
Nikolay Varlamov <varlamow.col@yahoo.com>
2021-03-04 18:11:46 +0300

    Upload photo

--- src/services/vk/photos.go
+++ src/services/vk/photos.go
@@ -1,12 +1,33 @@
 package vk
 
-import "github.com/go-vk-api/vk"
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"os"
+
+	"github.com/go-vk-api/vk"
+)
 
 // WallUploadServer structer which you get from GetWallUploadServer method
 type WallUploadServer struct {
 	UploadURL string `json:"upload_url"`
 	AlbumID   int    `json:"album_id"`
 	UserID    int    `json:"user_id"`
+}
+
+// PhotoUploadResult structer which you get from UploadPhoto method
+type PhotoUploadResult struct {
+	Server int    `json:"server"`
+	Photo  string `json:"photo"`
+	Hash   string `json:"hash"`
+}
+
+// PhotoPost .
+type PhotoPost struct {
+	photo *os.File
 }
 
 // GetWallUploadServer returns address of server for photos uploading
@@ -21,4 +42,47 @@ func (c *Client) GetWallUploadServer() *WallUploadServer {
 		&wallUploadServer,
 	)
 	return &wallUploadServer
+}
+
+//UploadPhoto uploads your photo to the provided VK file storage
+func (c *Client) UploadPhoto(url, filePath string) (PhotoUploadResult, error) {
+	img, _ := os.Open(filePath)
+	var b bytes.Buffer
+	w := multipart.NewWriter(&b)
+	fw, err := w.CreateFormFile("photo", filePath)
+	if err != nil {
+		return PhotoUploadResult{}, err
+	}
+	if _, err = io.Copy(fw, img); err != nil {
+		return PhotoUploadResult{}, err
+	}
+	w.Close()
+
+	req, err := http.NewRequest("POST", url, &b)
+	if err != nil {
+		return PhotoUploadResult{}, err
+	}
+	req.Header.Set("Content-Type", w.FormDataContentType())
+
+	// Submit the request
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return PhotoUploadResult{}, err
+	}
+
+	// resp
+	uplRes := PhotoUploadResult{}
+	dec := json.NewDecoder(res.Body)
+	err = dec.Decode(&uplRes)
+	if err != nil {
+		return PhotoUploadResult{}, err
+	}
+	defer res.Body.Close()
+
+	return PhotoUploadResult{
+		Server: uplRes.Server,
+		Photo:  uplRes.Photo,
+		Hash:   uplRes.Hash,
+	}, nil
 }
--- src/utils/utils.go
+++ src/utils/utils.go
@@ -1,9 +1,12 @@
 package utils
 
 import (
+	"encoding/json"
 	"io"
 	"net/http"
 	"os"
+
+	"github.com/go-resty/resty/v2"
 )
 
 // DownloadFile is a function for downloading file by its url
@@ -12,14 +15,31 @@ func DownloadFile(filepath string, url string) error {
 	if err != nil {
 		return err
 	}
-	resp.Body.Close()
+	defer resp.Body.Close()
 
 	out, err := os.Create(filepath)
 	if err != nil {
 		return err
 	}
-	out.Close()
+	defer out.Close()
 
 	_, err = io.Copy(out, resp.Body)
 	return err
+}
+
+// Upload file
+func Upload(uploadURL, field, filename string, reader io.Reader, holder interface{}) (interface{}, error) {
+	client := resty.New()
+	resp, err := client.R().
+		SetFileReader(field, filename, reader).
+		Post(uploadURL)
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal(resp.Body(), holder)
+	if err != nil {
+		return nil, err
+	}
+	return holder, nil
 }
