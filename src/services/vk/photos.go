1c1142c2d8eef63def177cc0ca50e08ef709c811
Nikolay Varlamov <varlamow.col@yahoo.com>
2021-03-04 15:06:32 +0300

    WallUploadServer base

--- src/services/vk/photos.go
+++ src/services/vk/photos.go
@@ -1,1 +1,13 @@
 package vk
+
+// WallUploadServer structer which you get from GetWallUploadServer method
+type WallUploadServer struct {
+	UploadURL string
+	AlbumID   int
+	UserID    int
+}
+
+// GetWallUploadServer returns address of server for photos uploading
+func (c *Client) GetWallUploadServer() {
+
+}
