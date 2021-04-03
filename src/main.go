2adc02299fe99ed18152897f4f4518759aaea2af
Nikolay <varlamow.col@yahoo.com>
2021-04-03 14:44:52 +0300

    cosmetic fix

--- src/main.go
+++ src/main.go
@@ -3,16 +3,5 @@ package main
 import "reddit-to-vk-auto-poster/src/services/script"
 
 func main() {
-	// godotenv.Load()
-	// redditclient := reddit.Initialize()
-	// posts, _ := redditclient.GetPosts()
-	// now := time.Now().Unix() + 60
-	// vkclient, _ := vk.Initialize()
-	// for index, post := range posts {
-	// 	err := vkclient.PostRedditSubmission(post, now+int64(index*3600))
-	// 	if err != nil {
-	// 		fmt.Print(err.Error())
-	// 	}
-	// }
 	script.Exec()
 }
